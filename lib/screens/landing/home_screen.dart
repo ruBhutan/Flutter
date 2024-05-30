e44dce3cbb686359e20c44cfa3c8c2e44a38cb3e
Rosan Yonghang <roshanlimbu714@gmail.com>
2024-05-30 12:27:25 +0600

    Feature: Integrate transactions api

--- lib/screens/landing/home_screen.dart
+++ lib/screens/landing/home_screen.dart
@@ -27,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
         children: [
           SummaryHome(),
           SummaryAccounts(),
-          // SummaryTransactions()
+          SummaryTransactions()
         ],
       ),
     );
